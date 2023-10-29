3365231dcfe35c5e18bb62df2730616c10d0b9e7
Collin Leavitt <zanthor0128@gmail.com>
2023-10-29 13:25:08 -0400

    Function created for validing input and looping a message

--- MtgRankSystem/MtgRankSystem/MtgRankSystem.cpp
+++ MtgRankSystem/MtgRankSystem/MtgRankSystem.cpp
@@ -3,23 +3,31 @@
 
 #include <iostream>
 
+char VaildateAnswer(char* answer, std::string message);
+
+static char yes = 'Y';
+static char no = 'N';
+
+static std::string questionAnswerString = "\n Y) Yes \n N) No\n";
+
 int main()
 {
     bool running = true;
     std::string deckName;
+    char comDraw;
 
     while (running)
     {
         std::cout << "What is the name of your deck?\n";
-
         std::cin >> deckName;
 
-        std::cout << "Do you wish to tell me another deck's name?\n Y) Yes \n N) No\n";
+        VaildateAnswer(&comDraw, "\nDoes yor commamder have card draw on it?");
 
         char answer;
-        std::cin >> answer;
 
-        if (toupper(answer) == 'N')
+        VaildateAnswer(&answer, "\nDo you wish to tell me another deck's name?");
+
+        if (answer == no)
         {
             running = false;
         }
@@ -28,13 +36,25 @@ int main()
     std::cout << "\n\nRanking System Shutting Down......\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+char VaildateAnswer(char *answer, std::string message)
+{
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    bool checkAgain = false;
+    do
+    {
+        checkAgain = false;
+
+        std::cout << message << questionAnswerString;
+        std::cin >> *answer;
+
+        *answer = toupper(*answer);
+        if (*answer != yes && *answer != no)
+        {
+            std::cout << *answer << " is not a validate answer. Please try again valiad answers are " << yes << " and " << no << ".....\n";
+            checkAgain = true;
+        }
+
+    } while (checkAgain);
+
+    return *answer;
+}
